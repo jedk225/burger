364440cf3dc366803c8c0fc0657e6e1aa48ccec4
jedi225 <jedtk@uci.edu>
2018-09-28 00:07:02 -0700

    setup the code to connect Node to MySQL

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+//code to read and set any environment variables with the dotenv package
+require('dotenv').config();
+
+// Grabs password from .env file so it does not get posted to Github
+var mypassword = process.env.MYSQL_PW;
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: mypassword,
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
