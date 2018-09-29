fc6835e8a0c9bf432687ea4d67dfeeb928f83eef
jedi225 <jedtk@uci.edu>
2018-09-29 00:11:21 -0700

    added burger controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,47 @@
+// Require express
+var express = require("express");
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger");
+
+// Create the router for the app
+var router = express.Router();
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne([
+        "burgerName"
+    ], [
+            req.body.burgerName
+        ], function (result) {
+            // select all burgers and display
+            res.redirect('/');
+        });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function (result) {
+        res.redirect('/');
+
+    });
+});
+
+//------------------------------------------------------------------------------------------------------
+// Export routes for server.js to use.
+module.exports = router;
