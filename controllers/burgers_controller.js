d5d5784a157f04cac54ac21d99260a086842e83f
jedi225 <jedtk@uci.edu>
2018-09-29 03:03:39 -0700

    updated the views

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,11 @@
 // Require express
 var express = require("express");
 
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger");
-
 // Create the router for the app
 var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
