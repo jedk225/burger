1ee030d40382907d1bfc004c19bbb448e14f652d
jedi225 <jedtk@uci.edu>
2018-09-29 18:11:53 -0700

    working UPDATE for devour

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -34,12 +34,27 @@ router.put("/api/burgers/:id", function (req, res) {
 
     console.log("condition", condition);
 
-    burger.updateOne({
-        devoured: true
-    }, condition, function (result) {
-        res.redirect('/');
+    // burger.updateOne({
+    //     devoured: true
+    // }, condition, function (result) {
+    //     res.redirect('/');
 
-    });
+    // });
+
+    burger.updateOne(
+        {
+          devoured: true
+        },
+        condition,
+        function(result) {
+          if (result.changedRows === 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+          }
+          res.status(200).end();
+    
+        }
+      );
 });
 
 //------------------------------------------------------------------------------------------------------
---
+++ public/assets/js/burger-public.js
@@ -0,0 +1,37 @@
+$(function () {
+    $(".devour").on("click", function (event) {
+        var id = $(this).data("id");
+        var devour = $(this).data("devour");
+        // alert(id)
+        // alert(devour)
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: { devoured: devour }
+        }).then(
+            function () {
+                console.log("changed devoured to", devour);
+                location.reload();
+            }
+        )
+    })
+
+    $(".addBurger").on("click", function (event) {
+        var addNew = $("#burger").val().trim();
+        alert(addNew)
+        if (addNew) {
+            $.ajax("/api/burgers/", {
+                type: "POST",
+                data: { name: addNew }
+            }).then(
+                function () {
+                    console.log("added new burger!");
+                    location.reload();
+                }
+            )
+        } else {
+            event.preventDefault();
+            alert("Please input a valid name")
+        }
+    })
+})
